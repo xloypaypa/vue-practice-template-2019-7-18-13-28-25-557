a2fa7a50fc8df18ac74cb8941bc6cb7c24263493
suxin <xloypaypa@gmail.com>
2019-07-23 10:31:14 +0800

    add getter.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-<div>
-<CounterGroup></CounterGroup>
-</div>
+    <div>
+        <CounterGroup></CounterGroup>
+        <h1>{{$store.getters.getCoooool()}}</h1>
+    </div>
 </template>
 
 <script>
 
-import CounterGroup from './components/CountGroup.vue'
-export default {
-  name: "app",
-  data: function() {
-    return{
-    message:0
+    import CounterGroup from './components/CountGroup.vue'
+
+    export default {
+        name: "app",
+        data: function () {
+            return {
+                message: 0
+            };
+        },
+        components: {
+            CounterGroup
+        }
     };
-  },
-  components:{
-    CounterGroup
-  }
-};
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        coooool: 7,
+    },
+    getters: {
+        getCoooool: (state) => () => {
+            return state.coooool - (state.coooool % 5);
+        }
+    }
+
+});
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    store
 }).$mount('#app')
